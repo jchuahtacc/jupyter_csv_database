d37447471512151454a033f13acd8ce65dc3849c
jchuahtacc <jchuah@tacc.utexas.edu>
2016-10-10 18:38:57 -0500

    Field select function

--- db_helper/database.py
+++ db_helper/database.py
@@ -1,16 +1,28 @@
 import pandas
-# import qgrid
+from qgrid import show_grid
 import ipywidgets as widgets
 from IPython.display import display
 class Database:
-    _column_checks = None
-    _item_layout = widgets.Layout(width='15%')
-    _box_layout = widgets.Layout(display='flex', flex_flow='row wrap')
-
+    _csv = None
+    _fields_grid = None
+    _df = None
     def __init__(self, csv):
-        df = pandas.read_csv(csv)
-        self._column_checks = tuple(widgets.Checkbox(description=field, value=True, layout=self._item_layout) for field in list(df.columns.values))
-        box = widgets.Box(children = self._column_checks, layout=self._box_layout)
-        display(box)
+        self._csv = csv
+        self._df = pandas.read_csv(csv)
+        fields = pandas.Series([ field for field in self._df.columns.values], name="Fields")
+        raw_cat = pandas.Categorical([ "No" for field in self._df.columns.values], categories=["Yes", "No"])
+        show = pandas.Series(raw_cat, name="Show")
 
+        df2 = pandas.concat([fields, show], axis=1)
+        display(widgets.HTML("<h3>Jupyter CSV Database</h3><video autoplay loop controls style='float: right'><source src='./db_helper/select_fields.mp4' type='video/mp4'></video><p>First, please choose the fields you wish to view in the table below. You may double click on the \"no\" next to a field name, select \"Yes\" and then click on a different cell to change that field's selection. You may also use the filter controls to search for fields. When you are finished, press OK at the bottom of this cell's output.</p>"))
+        self._fields_grid = show_grid(df2)
 
+        ok = widgets.Button(description="OK")
+        ok.on_click(self._fields_ok)
+        display(ok)
+
+    def _fields_ok(self, b):
+        self._fields_grid.export_all(self._fields_export_callback)
+
+    def _fields_export_callback(self, fields_df):
+        print self._df[fields_df.loc[fields_df["Show"] == "Yes"]["Fields"].values]
