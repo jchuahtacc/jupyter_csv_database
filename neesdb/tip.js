5217f26b2f732a7860414c8b95217b060b142759
jchuahtacc <jchuah@tacc.utexas.edu>
2016-11-17 17:08:48 -0600

    Removed header, button, video, code from tip.js

--- neesdb/tip.js
+++ neesdb/tip.js
@@ -12,18 +12,13 @@ define(['jquery', path ], function($, widget) {
         render: function() {
             TipWidgetView.__super__.render.apply(this, arguments);
 
-            this.$view = $("<div style='border-left: 6px solid #ccc!important; background-color: #ddddff!important; padding: 0.01em 16px; padding-bottom:   16px;'><h4>neesdb tip:</h4><table style='width: 100%;'><tr><td data-role='left' style='vertical-align: top; padding-right: 10px;'><p data-role='tip' class='hidden'></p><br /><code data-role='code' class='hidden'></code><br /><button class='btn btn-default'>Ok!</button></td><td data-role='right'><video style='width: 100%;' data-role='video' autoplay loop controls style='float: right' class='hidden'><source data-role='src' type='video/mp4'></video></td></tr></table></div>");
+            this.$view = $("<div style='border-left: 6px solid #ccc!important; background-color: #ddddff!important; padding: 0.01em 16px; padding-bottom:   16px;'><br /><p data-role='tip' class='hidden'></p></div>");
             $(this.el).append(this.$view);
             this.listenTo(this.model, 'change:tip', this.tip_changed, this);
             this.listenTo(this.model, 'change:code', this.code_changed, this);
             this.listenTo(this.model, 'change:src', this.src_changed, this);
             this.tip_changed();
-            this.code_changed();
-            this.src_changed();
             var that = this;
-            this.$view.find('button').click(function() {
-                that.send({ 'msg' : 'close' });
-            });
         },
 
         tip_changed: function() {
@@ -35,28 +30,6 @@ define(['jquery', path ], function($, widget) {
             }
         },
 
-        code_changed: function() {
-            var code = this.model.get('code');
-            if (code && code.length > 0) {
-               this.$view.find("[data-role='code']").html(code).removeClass("hidden");
-            } else {
-               this.$view.find("[data-role='code']").html("").addClass("hidden");
-            } 
-        },
-
-        src_changed: function() {
-            var src = this.model.get('src');
-            if (src && src.length > 0) {
-                this.$view.find("[data-role='left']").css('width', '50%');
-                this.$view.find("[data-role='right']").css('width', '50%');
-                this.$view.find("[data-role='src']").attr('src', '/nbextensions/neesdb/tips/' + src);
-                this.$view.find("[data-role='video']").removeClass("hidden");
-            } else {
-                this.$view.find("[data-role='left']").css('width', '100%');
-                this.$view.find("[data-role='right']").css('width', '0%');
-                this.$view.find("[data-role='video']").addClass("hidden");
-            }
-        }
     });
 
     return {
